c75c2cf17bb35b17af28ff7e31d06cb4c9f57510
Jeff Smith <smith734@msu.edu>
2010-11-06 11:21:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beermee::Application.config.secret_token = '3029bc70bae9b26a4e5ecb4d2c57719250d303c46e7db1419ce0b9981028cb319326f6d315c6f663b63d4e69149b60ed6f38c492fc4656272e4c59fe76006550'
