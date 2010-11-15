9059a615b319e8624139dd11f299f9426546652a
Jeff Smith <smith734@msu.edu>
2010-11-14 18:08:01 -0800

    Added javascript to show state field only when selected country is United States

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var usa = "214";
+
+jQuery(function($) {
+  // Only show State field if selected country is United States
+  $("select#brewery_country_id").change(function() {
+    if ($("select#brewery_country_id option:selected").val() != usa) {
+      $("div#state").hide();
+    } 
+    else {
+      $("div#state").show();
+    }
+  });
+});
