00708756029819dd5075a85fedbbd9cbdd41d827
Jeff Smith <smith734@msu.edu>
2010-12-04 17:45:40 -0800

    added user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @attr = { :email => "test@test.com",
+              :password => "password",
+              :password_confirmation => "password" }
+  end
+
+  describe "validations" do
+    
+    it "should require an email" do
+      user = User.new(@attr.merge(:email => ""))
+      user.should_not be_valid
+    end
+
+    it "should have a valid email" do
+      user = User.new(@attr.merge(:email => "foobar"))
+      user.should_not be_valid
+    end
+
+    it "should require a password" do
+      user = User.new(@attr.merge(:password => ""))
+      user.should_not be_valid
+    end
+
+    it "should reject a password less than 6 characters long" do
+      user = User.new(@attr.merge(:password => "foo"))
+      user.should_not be_valid
+    end
+
+    it "should reject a password greater than 20 characters" do
+      user = User.new(@attr.merge(:password => "a" * 21))
+      user.should_not be_valid
+    end
+
+    it "should require a password confirmation" do
+      user = User.new(@attr.merge(:password_confirmation => ""))
+      user.should_not be_valid
+    end
+
+    it "should reject different password and password confirmation" do
+      user = User.new(@attr.merge(:password_confirmation => "foobar"))
+      user.should_not be_valid
+    end
+  end
 end
