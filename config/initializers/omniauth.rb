4244b2530b18a5078088c93bdd2586c04d25139e
Jeff Smith <smith734@msu.edu>
2011-02-17 18:47:29 -0500

    removed api keys and secrets before making repo public

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter,  'wykFnIdD1ngUQw1gvskvTA', 'SurmCDlY2OEowGeblUE8iioFd8VoIsCLsGopGhSGEQ'
-  provider :facebook, '174750099211472', '5ddfc117adb8cc144111cce89346dfb4'
+  provider :twitter,  'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :facebook, 'APP_ID', 'APP_SECRET'
 end
